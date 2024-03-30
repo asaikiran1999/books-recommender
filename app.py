b004e04f95cdcb0db654aea8dd2050ba246e9061
Avasarala Sai Kiran <58164887+asaikiran1999@users.noreply.github.com>
2024-03-31 00:42:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -120,10 +120,10 @@ elif page == "selected book recommender":
 	st.text('The Secret Life of Bees')
 	st.text('Girl with a Pearl Earring')
 	st.text('About a Boy')
-	if st.button('recommend'):
-		recommended_books = recommend(input)
-                for book_image_url in recommended_books:
-			st.image(book_image_url, width=200)
-			book_title = books[books['Image-URL-M'] == book_image_url]['Book-Title'].values[0]
-			st.write(book_title)
+recommended_books = recommend(input)
+if st.button('recommend'):
+	for book_image_url in recommended_books:
+		st.image(book_image_url, width=200)
+		book_title = books[books['Image-URL-M'] == book_image_url]['Book-Title'].values[0]
+		st.write(book_title)
 
