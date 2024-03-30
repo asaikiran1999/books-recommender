c9e612f1bf24ec659e675b2117152fdab7f9709b
Avasarala Sai Kiran <58164887+asaikiran1999@users.noreply.github.com>
2024-03-31 01:33:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,31 +23,31 @@ books = books1.copy()
 page = st.selectbox("Choose your page", ["Top 50 book recommendation", "selected book recommender"])
 def paginator(label, items, items_per_page=10, on_sidebar=True):
 # Figure out where to display the paginator
-if on_sidebar:
-    location = st.sidebar.empty()
-else:
-    location = st.empty()
+	if on_sidebar:
+	    location = st.sidebar.empty()
+	else:
+	    location = st.empty()
 
 # Display a pagination selectbox in the specified location.
-items = list(items)
-n_pages = len(items)
-n_pages = (len(items) - 1) // items_per_page + 1
-page_format_func = lambda i: "Page %s" % i
-page_number = location.selectbox(label, range(n_pages), format_func=page_format_func)
+	items = list(items)
+	n_pages = len(items)
+	n_pages = (len(items) - 1) // items_per_page + 1
+	page_format_func = lambda i: "Page %s" % i
+	page_number = location.selectbox(label, range(n_pages), format_func=page_format_func)
 
 # Iterate over the items in the page to let the user display them.
-min_index = page_number * items_per_page
-max_index = min_index + items_per_page
-import itertools
-return itertools.islice(enumerate(items), min_index, max_index)
+	min_index = page_number * items_per_page
+	max_index = min_index + items_per_page
+	import itertools
+	return itertools.islice(enumerate(items), min_index, max_index)
 
 if page == "Top 50 book recommendation":
-items2 = []
-items3 = []
-x = pd.read_csv("https://raw.githubusercontent.com/asaikiran1999/books-recommender/main/data/top50book.csv")
-for i in range(50):
-items2.append(x['Image-URL-M'][i])
-items3.append(x['Book-Title'][i])
+	items2 = []
+	items3 = []
+	x = pd.read_csv("https://raw.githubusercontent.com/asaikiran1999/books-recommender/main/data/top50book.csv")
+	for i in range(50):
+	items2.append(x['Image-URL-M'][i])
+	items3.append(x['Book-Title'][i])
 
 image_iterator = paginator("Select next page", zip(items2, items3))
 indices_on_page, images_and_titles_on_page = map(list, zip(*image_iterator))
