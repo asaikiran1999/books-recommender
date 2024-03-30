5ef2929f0b4660c2933575f66d299ad486d82e32
Avasarala Sai Kiran <58164887+asaikiran1999@users.noreply.github.com>
2024-03-31 02:30:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,32 +30,57 @@ def paginator(label, items, items_per_page=10, on_sidebar=True):
 	    location = st.sidebar.empty()
 	else:
 	    location = st.empty()
+# Display a pagination selectbox in the specified location.
+	items = list(items)
+	n_pages = len(items)
+	n_pages = (len(items) - 1) // items_per_page + 1
+	page_format_func = lambda i: "Page %s" % i
+	page_number = location.selectbox(label, range(n_pages), format_func=page_format_func)
+# Iterate over the items in the page to let the user display them.
+	min_index = page_number * items_per_page
+	max_index = min_index + items_per_page
+	import itertools
+	return itertools.islice(enumerate(items), min_index, max_index)
 
+if page == "Top 50 book recommendation":
+	items2 = []
+	items3 = []
+	x = pd.read_csv("https://raw.githubusercontent.com/asaikiran1999/books-recommender/main/data/top50book.csv")
+	for i in range(50):
+                items2.append(x['Image-URL-M'][i])
+                items3.append(x['Book-Title'][i])
+	image_iterator = paginator("Select next page", zip(items2, items3))
+	indices_on_page, images_and_titles_on_page = map(list, zip(*image_iterator))
+
+	for image_url, book_title in images_and_titles_on_page:
+		st.image(image_url, width=200)
+		st.write(book_title)
+elif page == "selected book recommender":	
 # Load data for selected book recommender
-users = pd.read_csv("https://raw.githubusercontent.com/asaikiran1999/books-recommender/main/data/Users.csv")
-ratings = pd.read_csv("https://raw.githubusercontent.com/asaikiran1999/books-recommender/main/data/Ratings.csv")
-ratings_and_books = ratings.merge(books, on='ISBN')
-x = ratings_and_books.groupby('User-ID').count()['Book-Rating'] > 200
-best_users = x[x].index
-filtered_ratings = ratings_and_books[ratings_and_books['User-ID'].isin(best_users)]
-y = filtered_ratings.groupby('Book-Title').count()['Book-Rating'] >= 50
-famous_books = y[y].index
-filtered_famous = filtered_ratings[filtered_ratings['Book-Title'].isin(famous_books)]
-pivot = filtered_famous.pivot_table(index='Book-Title', columns='User-ID', values='Book-Rating')
-pivot.fillna(0, inplace=True)
+	users = pd.read_csv("https://raw.githubusercontent.com/asaikiran1999/books-recommender/main/data/Users.csv")
+	ratings = pd.read_csv("https://raw.githubusercontent.com/asaikiran1999/books-recommender/main/data/Ratings.csv")
+	ratings_and_books = ratings.merge(books, on='ISBN')
+	x = ratings_and_books.groupby('User-ID').count()['Book-Rating'] > 200
+	best_users = x[x].index
+	filtered_ratings = ratings_and_books[ratings_and_books['User-ID'].isin(best_users)]
+	y = filtered_ratings.groupby('Book-Title').count()['Book-Rating'] >= 50
+	famous_books = y[y].index
+	filtered_famous = filtered_ratings[filtered_ratings['Book-Title'].isin(famous_books)]
+	pivot = filtered_famous.pivot_table(index='Book-Title', columns='User-ID', values='Book-Rating')
+	pivot.fillna(0, inplace=True)
 
 # User input and recommendation
-user_input = st.text_input("Book Name", "")
-st.text('Most searched books are:')
-st.text("Harry Potter and the Sorcerer's Stone (Book 1)")
-st.text('The Da Vinci Code')
-st.text('The Secret Life of Bees')
-st.text('Girl with a Pearl Earring')
-st.text('About a Boy')
-
-if st.button('Recommend'):
-    recommended_books = recommend(user_input, pivot)
-    for book_image_url in recommended_books:
-        st.image(book_image_url, width=200)
-        book_title = books[books['Image-URL-M'] == book_image_url]['Book-Title'].values[0]
-        st.write(book_title)
+	user_input = st.text_input("Book Name", "")
+	st.text('Most searched books are:')
+	st.text("Harry Potter and the Sorcerer's Stone (Book 1)")
+	st.text('The Da Vinci Code')
+	st.text('The Secret Life of Bees')
+	st.text('Girl with a Pearl Earring')
+	st.text('About a Boy')
+	
+	if st.button('Recommend'):
+	    recommended_books = recommend(user_input, pivot)
+	    for book_image_url in recommended_books:
+	        st.image(book_image_url, width=200)
+	        book_title = books[books['Image-URL-M'] == book_image_url]['Book-Title'].values[0]
+	        st.write(book_title)
